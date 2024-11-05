467ab125cb0f58493ffed0bc314536e39b7e3b69
Manisha S K <manishask2003@gmail.com>
2024-11-05 14:41:44 +0530

    adminController and admin route

---
+++ backend/routes/adminRoute.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { addDoctor, loginAdmin,} from '../controllers/adminController.js';
+import upload from '../middleware/multer.js';
+
+
+
+const adminRouter = express.Router();
+
+
+
+adminRouter.post("/add-doctor",upload.single('image'), addDoctor)
+adminRouter.post("/login", loginAdmin)
+
+export default adminRouter;
+
+
